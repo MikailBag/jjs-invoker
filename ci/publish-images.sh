051b80713d148dd025b4eb43e228a9c39401fa39
Mikail Bagishov <bagishov.mikail@yandex.ru>
2021-04-24 16:34:31 +0300

    tmp

--- ci/publish-images.sh
+++ ci/publish-images.sh
@@ -11,6 +11,6 @@ else
   echo "unknown GITHUB_REF: $GITHUB_REF"
   exit 1
 fi
-echo ${{ secrets.GHCR_TOKEN }} | docker login ghcr.io -u $GITHUB_ACTOR --password-stdin
+echo ${{ secrets.GITHUB_TOKEN }} | docker login ghcr.io -u $GITHUB_ACTOR --password-stdin
 docker tag jjs-invoker ghcr.io/jjs-dev/jjs-invoker:$TAG
 docker push ghcr.io/jjs-dev/jjs-invoker:$TAG
