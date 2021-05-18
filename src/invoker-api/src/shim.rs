39c99e2325f97b4bd7bfb1fc6e92e260213f62e0
Mikail Bagishov <bagishov.mikail@yandex.ru>
2021-05-18 11:05:19 +0300

    Support executable extraFiles

--- src/invoker-api/src/shim.rs
+++ src/invoker-api/src/shim.rs
@@ -6,6 +6,18 @@ use std::collections::HashMap;
 
 pub const EXTRA_FILES_DIR_NAME: &str = "EXTRA_FILES";
 
+/// See RequestExtensions::extra_files
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+#[serde(deny_unknown_fields)]
+pub struct ExtraFile {
+    /// File contents
+    pub contents: InputSource,
+    /// Make file executable
+    #[serde(default)]
+    pub executable: bool,
+}
+
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 #[serde(deny_unknown_fields)]
@@ -14,7 +26,7 @@ pub struct RequestExtensions {
     /// This files will be written to directory that can be
     /// referred in mounts as value of the EXTRA_FILES_DIR_NAME constant.
     #[serde(default)]
-    pub extra_files: HashMap<String, InputSource>,
+    pub extra_files: HashMap<String, ExtraFile>,
     /// Values to substitute into `commands`.
     /// Keys will be automatically prefixed with `Request.`.
     #[serde(default)]
